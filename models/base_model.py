2b01c22c71c4d05f790131941c470597355b64e7
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-26 18:45:41 +0000

    task 4 implemented, create from dict

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ import uuid
 class BaseModel():
     """ temp """
 
-    def __init___(self):
+    def __init__(self, *args, **kwargs):
         """initializes BaseModel"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         classname = self.__class__.__name__
