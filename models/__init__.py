7e661c1e1e207ee8bdc3fe9d4d7c22d7dc882973
amateurdullahan <zabrak12@gmail.com>
2020-06-26 20:11:44 +0000

    filestorage created, init and basemodel updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,4 @@
-""" blank """
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,21 @@
 #!/bin/usr/python3
-""" temp text """
+"""file containing basemodel class"""
 from datetime import datetime
+from models import storage
 import uuid
 
 
 class BaseModel():
-    """ temp """
+    """Class BaseModel, base model for AirBnB Clone"""
 
     def __init__(self, *args, **kwargs):
         """initializes BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs is not None:
+        if kwargs is None or len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -21,15 +24,18 @@ class BaseModel():
                     setattr(self, key, value)
 
     def __str__(self):
+        """class str method"""
         classname = self.__class__.__name__
         return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
         """updates updated_at with current time"""
+        storage.save()
         self.updated_at = datetime.now()
 
 
     def to_dict(self):
+        """creates a dictionary of BaseModel"""
         self.dictionary = dict(self.__dict__)
         self.dictionary['__class__'] = self.__class__.__name__
         self.dictionary['created_at'] = datetime.isoformat(self.created_at)
