99bd3346a7cb0cfc45b4cc61a09bb89092a9849e
amateurdullahan <zabrak12@gmail.com>
2020-07-01 21:54:11 +0000

    added 2 more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -34,3 +34,57 @@ class TestFileStorage(unittest.TestCase):
         if os.path.exists('file.json'):
             print('file still exists')
             os.remove('file.json')
+
+    def test_all(self):
+        """test all"""
+        fs2 = FileStorage()
+        list_app = []
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        for exists in fs2.all().keys():
+            list_app.append(fs2.all()[exists])
+        for exists in list_app:
+            del fs2.all()[exists.__class__.__name__ + '.' + exists.id]
+
+        self.assertIsInstance(fs2.all(), dict)
+        self.assertEqual(fs2.all(), {})
+        bm1, bm2 = BaseModel(), BaseModel()
+        fs2.new(bm1)
+        fs2.new(bm2)
+        self.assertEqual(fs2.all(), {'BaseModel.' + bm1.id : bm1,
+                                     'BaseModel.' + bm2.id : bm2})
+
+        del bm1, bm2, fs2
+
+    def test_new(self):
+        """ test new """
+        dic = {"id": "8d8b4200-z106-469d-aec9-70zae1224150",
+               "__class__": "BaseModel",
+               "updated_at": "2020-07-01T16:47:21.260793",
+               "created_at": "2020-07-01T16:47:21.260752"}
+        fs3 = FileStorage()
+        list_app = []
+        if os.path.exists('file.json'):
+            os.remove('file.json')
+        for exists in fs3.all().keys():
+            list_app.append(fs3.all()[exists])
+        for exists in list_app:
+            del fs3.all()[exists.__class__.__name__ + '.' + exists.id]
+        classes = [Amenity(**dic), BaseModel(**dic), City(**dic), Place(**dic),
+                   Review(**dic), State(**dic), User(**dic)]
+
+        for cls in classes:
+            fs3.new(cls)
+            self.assertIn(cls.__class__.__name__ + '.' + cls.id,
+                          fs3.all())
+
+        for cls in classes:
+            name = cls.__class__.__name__
+            self.assertTrue(name + '.' + cls.id in fs3.all().keys())
+
+        for i in range(len(fs3.all().keys())):
+            self.assertIn(fs3.all()[list(fs3.all().keys())[i]],
+                          classes)
+        for exists in classes:
+            del exists
+        del fs3
